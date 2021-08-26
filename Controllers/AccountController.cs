5e18f9f95bec47c6707d991585e8c2273f188252
avel40m <63351751+avel40m@users.noreply.github.com>
2021-08-26 13:14:51 -0300

    primer

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,40 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Login.Controllers
+{
+    [Route("Account")]
+    public class AccountController : Controller
+    {
+        [Route("")]
+        [Route("index")]
+        [Route("~/")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [Route("login")]
+        [HttpPost]
+        public IActionResult Login(string username,string password)
+        {
+            if(username != null && password != null && username.Equals("acc1") && password.Equals("123"))
+            {
+                HttpContext.Session.SetString("username",username);
+                return View("Success");
+            }
+            else
+            {
+                ViewBag.error = "Invalid Account";
+                return View("Index");
+            }
+        }
+
+        [Route("logout")]
+        [HttpGet]
+        public IActionResult Logout()
+        {
+            HttpContext.Session.Remove("username");
+            return RedirectToAction("Index");
+        }
+    }
+}
